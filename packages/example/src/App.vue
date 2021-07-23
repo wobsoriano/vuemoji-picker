20b3efde54bc4e6664128a17ae74a79f9863f27e
Robert Soriano <sorianorobertc@gmail.com>
2021-07-24 02:21:29 +0800

    remove init vue component

--- packages/example/src/App.vue
+++ packages/example/src/App.vue
@@ -1,17 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 })
 </script>
 
