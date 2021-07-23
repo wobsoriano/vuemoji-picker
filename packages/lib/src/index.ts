b8c0d1d46afe874f2259fa13cc996ec239edd819
Robert Soriano <sorianorobertc@gmail.com>
2021-07-24 03:54:51 +0800

    re-export types from emoji-picker-element

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -1,6 +1,18 @@
 import VuemojiPicker from './components/VuemojiPicker'
 import useDatabase from './composables/useDatabase'
 
+export type {
+    SkinTone,
+    NativeEmoji,
+    EmojiSkin,
+    DatabaseConstructorOptions,
+    PickerConstructorOptions,
+    I18n,
+    I18nCategories,
+    EmojiClickEventDetail,
+    SkinToneChangeEventDetail,
+    CustomEmoji
+} from 'emoji-picker-element/shared'
 export {
     VuemojiPicker,
     useDatabase
