f1f24f50c8ae2e86003a52e6c57f474941e621aa
wobsoriano <sorianorobertc@gmail.com>
2023-07-05 09:54:02 -0700

    simplify codebase

--- dev/vite.config.js
+++ dev/vite.config.js
@@ -1,8 +1,13 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/vuemoji-picker/' : './',
+  resolve: {
+    alias: {
+      'vuemoji-picker': path.resolve(__dirname, '../src'),
+    },
+  },
 })
